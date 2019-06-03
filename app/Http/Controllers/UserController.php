902cfcb178be213bcbbc9342cc9c1137156410a4
AnaiO <anais.berton.io@gmail.com>
2019-06-03 16:06:56 +0200

    formulaire validation inscription avec verifications filtre et hash

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ use App\AppUsers;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller 
 {
@@ -13,29 +14,48 @@ class UserController extends Controller
         return view('logForm');
     }
 
-    public function signupPost(Request $request, Response $response)
-    {
-        //if post input exist and email not already in database
-        $firstname   = isset($request->firstname)? htmlspecialchars($request->firstname) : "";
-        $lastname    = isset($request->lastname)? htmlspecialchars($request->lastname) : "";   
-        $email       = isset($request->email)? htmlspecialchars($request->email) : "";   
-        $password    = isset($request->password)? htmlspecialchars($request->password) : "";        
+    public function signupPost(Request $request)
+    {   
+        $firstname = $request->firstname;
+        $lastname = $request->lastname;
+        $email = $request->email;
+        $password = $request->password;
         
-        $emailAlreadyExist = AppUsers::where('email', $email)->get();
+
+        if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+            if(!empty($firstname)
+            && (!empty($lastname))
+            && (!empty($email))
+            && (!empty($password))){
+                $emailAlreadyExist = AppUsers::where('email', $email)->get();
        
-        if(count($emailAlreadyExist) != 0){
-            echo "deja enregistré";
+                if(count($emailAlreadyExist) != 0){
+                    echo "deja enregistré";
+                }else{
+                    $appUsers = new AppUsers;
+                    $appUsers->firstname = ucwords(strtolower($firstname));
+                    $appUsers->lastname = ucwords(strtolower($lastname));
+                    $appUsers->email = $email;
+                    $appUsers->password = Hash::make($password);
+                    $appUsers->status = 1;
+                    $appUsers->save();
+
+                    return redirect()->route('home');
+                }
+
+            }else{
+                echo "Tous les champs ne sont pas remplis";
+            }   
+            
         }else{
-            $appUsers = new AppUsers;
-            $appUsers->firstname = ucwords(strtolower($firstname));
-            $appUsers->lastname = ucwords(strtolower($lastname));
-            $appUsers->email = $email;
-            $appUsers->password = $password;
-            $appUsers->status = 1;
-            $appUsers->save();
+            echo "email invalide";
         }
 
-        return redirect()->route('home');
+        
+        
+        // dump($password);
+        
+        
         
 
     }
--- resources/views/logForm.php
+++ resources/views/logForm.php
@@ -8,12 +8,12 @@
             <form action="" method="post">
             <div class="form-group">
                     <label for="email">Email </label>
-                    <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp" placeholder="Email">
+                    <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp" placeholder="Email" required>
                     <small id="emailHelp" class="form-text text-muted">Nous ne communiquons jamais vos informations personnelles.</small>
                 </div>
                 <div class="form-group">
                     <label for="exampleInputPassword1">Mot de passe</label>
-                    <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+                    <input type="password" class="form-control" id="password" name="password" placeholder="Password" required>
                 </div>
                 <button type="submit" class="btn btn-primary">Sign in</button>
             </form>
@@ -29,21 +29,21 @@
             <div class="form-row">
                 <div class="form-group col-6">
                     <label for="lastname">Nom</label>
-                    <input type="text" class="form-control" id="lastname" name="lastname" placeholder="Nom">
+                    <input type="text" class="form-control" id="lastname" name="lastname" placeholder="Nom" required>
                 </div>
                 <div class="form-group col-6">
                     <label for="firstname">Prénom</label>
-                    <input type="text" class="form-control" id="firstname" name="firstname" placeholder="Prénom">
+                    <input type="text" class="form-control" id="firstname" name="firstname" placeholder="Prénom" required>
                 </div>
               </div>  
             <div class="form-group">
                 <label for="email">Email </label>
-                <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp" placeholder="Email">
+                <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp" placeholder="Email" required>
                 <small id="emailHelp" class="form-text text-muted">Nous ne communiquons jamais vos informations personnelles.</small>
             </div>
             <div class="form-group">
                 <label for="exampleInputPassword1">Mot de passe</label>
-                <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+                <input type="password" class="form-control" id="password" name="password" placeholder="Password" required>
             </div>
             <button type="submit" class="btn btn-primary">Submit</button>
         </form>
