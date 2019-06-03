1a3301b5d0edc721568e881320eff973afebe790
AnaiO <anais.berton.io@gmail.com>
2019-06-03 14:18:52 +0200

    Refonte QuizController, ajout indications

--- app/Http/Controllers/QuizController.php
+++ app/Http/Controllers/QuizController.php
@@ -2,38 +2,25 @@
 namespace App\Http\Controllers;
 
 use App\Quiz;
-use App\Answer;
-use App\Question;
 use App\Http\Controllers\Controller;
 
 class QuizController extends Controller 
 {
+    /**
+     * Quiz page
+     *
+     * @param int $id Quiz id
+     *
+     * @return \Illuminate\View\View
+     */
+
     public function showQuiz($id)
     {
         $quizInfos = Quiz::find($id);
 
-        // $questionsCollection = Question::where('quizzes_id', $id)
-        //                                 ->get();
-        // $wikiDistinct = Question::where('quizzes_id', $id)
-        //                         ->select('wiki')
-        //                         ->distinct()
-        //                         ->get();
-        // $randomWrongAnswers = Answer::where('questions_id', "!=", $id)
-        //                                 ->take(3)
-        //                                 ->inRandomOrder()
-        //                                 ->get();
-        //                             dump($randomWrongAnswers);
-
-
-
-        
 
         return view('quiz', [
             'quizInfos' => $quizInfos,
-            // 'questionsCollection' => $questionsCollection,
-            // 'wikiDistinct' => $wikiDistinct,
-            // 'randomWrongAnswers' => $randomWrongAnswers
-
         ]);
     }
 }
